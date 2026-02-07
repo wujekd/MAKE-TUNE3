a325e5b044cdb39601cf0a3d8c8e6eed34a73c32
wujekd <dom.wujek@gmail.com>
2026-02-07 23:03:27 +0000

    fix security rules for project creation

--- make-tune3-react/src/config.ts
+++ make-tune3-react/src/config.ts
@@ -1,7 +1,7 @@
 // Set to true to show debug tools (Store/Debug panels), false to hide
 export const SHOW_DEBUG_TOOLS = false;
 
-export const DEBUG_ALLOW_MULTIPLE_SUBMISSIONS = true;
+export const DEBUG_ALLOW_MULTIPLE_SUBMISSIONS = false;
 export const MAX_SUBMISSION_FILE_SIZE = 131072000;
 export const MAX_PDF_FILE_SIZE = 52428800;
 export const MAX_ZIP_FILE_SIZE = 314572800;
